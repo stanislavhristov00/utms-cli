037532eefa57056fe88b145f6de647757612acfa
Stanislav Hristov <stanislavhristov00@abv.bg>
2021-05-04 23:48:35 +0300

    Fixed naming of build file

--- src/main/java/com/vmware/finaltask/cli/utmsMain.java
+++ src/main/java/com/vmware/finaltask/cli/utmsMain.java
@@ -2,5 +2,6 @@ package com.vmware.finaltask.cli;
 
 public class utmsMain {
     public static void main(String[] args) {
+        System.out.println("This is the first command line argument" + args[0]);
     }
 }
